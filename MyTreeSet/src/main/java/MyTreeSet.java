0ee8ee6865bd5116548e5d128656cfa829a61db7
Dmitry <qudeed@gmail.com>
2017-11-14 06:31:54 +0300

    Initial commit.

---
+++ MyTreeSet/src/main/java/MyTreeSet.java
@@ -0,0 +1,457 @@
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.AbstractSet;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * A generic set implemented using binary search tree.
+ *
+ * @param <E> type of stored value
+ */
+public class MyTreeSet<E> extends AbstractSet<E> implements HisTreeSet<E> {
+    private Comparator<? super E> comparator;
+    private int size = 0;
+    private Node root = null;
+    private boolean reverse = false;
+
+    /**
+     * Makes new myTreeSet containing values from specified myTreeSet, but with reversed order view of the values.
+     *
+     * @param originalSet specified myTreeSet
+     */
+    private MyTreeSet(MyTreeSet<E> originalSet) {
+        root = originalSet.root;
+        size = originalSet.size;
+        comparator = originalSet.comparator;
+        reverse = !originalSet.reverse;
+    }
+
+    /**
+     * Returns Node storing specified value.
+     *
+     * @param value specified value
+     * @return found Node if there is such Node, and null otherwise.
+     */
+    @Nullable
+    private Node find(@NotNull E value) {
+        Node current = root;
+
+        while (current != null) {
+            int compareResult = comparator.compare(current.value, value);
+            if (compareResult == 0) {
+                return current;
+            } else if (compareResult > 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns lowest value.
+     *
+     * @return found value if there is such value, and null otherwise
+     */
+    private E normalOrderFirst() {
+        Node current = root;
+        if (current == null) {
+            return null;
+        }
+
+        while (current.left != null) {
+            current = current.left;
+        }
+
+        return current.value;
+    }
+
+    /**
+     * Returns highest value.
+     *
+     * @return found value if there is such value, and null otherwise
+     */
+    private E normalOrderLast() {
+        Node current = root;
+        if (current == null) {
+            return null;
+        }
+
+        while (current.right != null) {
+            current = current.right;
+        }
+
+        return current.value;
+    }
+
+    /**
+     * Returns the greatest value in this set strictly less than the specified value.
+     *
+     * @param value specified value
+     * @return found value if there is such value, and null otherwise
+     */
+    @Nullable
+    private E normalOrderLower(@NotNull E value) {
+        Node current = root;
+        if (current == null) {
+            return null;
+        }
+        E answer = null;
+
+        while (current != null) {
+            int compareResult = comparator.compare(current.value, value);
+            if (compareResult >= 0) {
+                current = current.left;
+            } else {
+                answer = current.value;
+                current = current.right;
+            }
+        }
+
+        return answer;
+    }
+
+    /**
+     * Returns the greatest value in this set less than or equal to the specified value,
+     *
+     * @param value specified value
+     * @return found value if there is such value, and null otherwise
+     */
+    @Nullable
+    private E normalOrderFloor(@NotNull E value) {
+        Node found = find(value);
+        return found != null ? found.value : normalOrderLower(value);
+    }
+
+    /**
+     * Returns the least value in this set greater than or equal to the specified value.
+     *
+     * @param value specified value
+     * @return found value if there is such value, and null otherwise
+     */
+    @Nullable
+    private E normalOrderCeiling(@NotNull E value) {
+        Node found = find(value);
+        return found != null ? found.value : normalOrderHigher(value);
+    }
+
+    /**
+     * Returns the least value in this set strictly greater than the given value,
+     *
+     * @param value specified value
+     * @return found value if there is such value, and null otherwise
+     */
+    @Nullable
+    private E normalOrderHigher(@NotNull E value) {
+        Node current = root;
+        E answer = null;
+
+        while (current != null) {
+            int compareResult = comparator.compare(current.value, value);
+            if (compareResult <= 0) {
+                current = current.right;
+            } else {
+                answer = current.value;
+                current = current.left;
+            }
+        }
+
+        return answer;
+    }
+
+    /**
+     * Initializes comparator for values ordering by specified comparator.
+     *
+     * @param comparator specified comparator
+     */
+    public MyTreeSet(@NotNull Comparator<? super E> comparator) {
+        this.comparator = comparator;
+    }
+
+    /**
+     * Initializes comparator for values ordering using value's comparability,
+     * therefore generic type must implements Comparable interface.
+     */
+    public MyTreeSet() {
+        comparator = (o1, o2) -> ((Comparable<E>) o1).compareTo(o2);
+    }
+
+    /**
+     * Checks if there is a value equals to specified value.
+     *
+     * @param value to check
+     * @return true if there is such value, and false otherwise
+     */
+    @Override
+    public boolean contains(@NotNull Object value) {
+        return find((E) value) != null;
+    }
+
+    /**
+     * Adds new Node storing specified value to the tree.
+     *
+     * @param value to add
+     * @return false if there was specified value, and true otherwise
+     */
+    @Override
+    public boolean add(@NotNull E value) {
+        if (contains(value)) {
+            return false;
+        }
+        if (root == null) {
+            root = new Node(value);
+            size = 1;
+            return true;
+        }
+
+        Node current = root;
+        Node parent = null;
+        while (current != null) {
+            parent = current;
+            int compareResult = comparator.compare(current.value, value);
+            if (compareResult > 0) {
+                current = current.left;
+            } else if (compareResult < 0) {
+                current = current.right;
+            }
+
+        }
+
+        int compareResult = comparator.compare(parent.value, value);
+        if (compareResult < 0) {
+            parent.right = new Node(value);
+        } else if (compareResult > 0) {
+            parent.left = new Node(value);
+        }
+
+        size++;
+        return true;
+    }
+
+    /**
+     * Removes Node storing specified value from tree.
+     *
+     * @param value to remove
+     * @return true if there was specified value, and false otherwise
+     */
+    @Override
+    public boolean remove(@NotNull Object value) {
+        if (!contains(value)) {
+            return false;
+        }
+        size--;
+
+        Node current = root;
+        Node parent = null;
+        int compareResult;
+        while ((compareResult = comparator.compare(current.value, (E) value)) != 0) {
+            parent = current;
+            if (compareResult > 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+
+        boolean isLeftChild = parent != null && parent.left == current;
+        if (parent != null) {
+            if (current.left == null) {
+                if (isLeftChild) {
+                    parent.left = current.right;
+                    return true;
+                } else {
+                    parent.right = current.right;
+                    return true;
+                }
+            } else if (current.right == null) {
+                if (isLeftChild) {
+                    parent.left = current.left;
+                    return true;
+                } else {
+                    parent.right = current.left;
+                    return true;
+                }
+            }
+        } else {
+            if (current.left == null) {
+                root = current.right;
+                return true;
+            } else if (current.right == null) {
+                root = current.left;
+                return true;
+            }
+        }
+
+        E nextValue = normalOrderHigher(current.value);
+        remove(nextValue);
+        current.value = nextValue;
+
+        return true;
+    }
+
+    /**
+     * Returns an iterator over the values in this set in descending order.
+     */
+    public Iterator<E> descendingIterator() {
+        return descendingSet().iterator();
+    }
+
+    /**
+     * Returns a reverse order view of the values contained in this set.
+     */
+    public MyTreeSet<E> descendingSet() {
+        return new MyTreeSet<>(this);
+    }
+
+    /**
+     * Returns lowest value if order isn't reversed, and highest value otherwise.
+     *
+     * @return found value if there is such value, and null otherwise
+     */
+    public E first() {
+        if (reverse) {
+            return normalOrderLast();
+        } else {
+            return normalOrderFirst();
+        }
+    }
+
+    /**
+     * Returns highest value if order isn't reversed, and lowest value otherwise.
+     *
+     * @return found value if there is such value, and null otherwise
+     */
+    public E last() {
+        if (reverse) {
+            return normalOrderFirst();
+        } else {
+            return normalOrderLast();
+        }
+    }
+
+    /**
+     * Returns the greatest value in this set strictly less than the specified value if order isn't reversed, and
+     * the least value in this set strictly greater than the specified value otherwise.
+     *
+     * @param value specified value
+     * @return found value if there is such value, and null otherwise
+     */
+    @Nullable
+    public E lower(@NotNull E value) {
+        if (reverse) {
+            return normalOrderHigher(value);
+        } else {
+            return normalOrderLower(value);
+        }
+    }
+
+    /**
+     * Returns the greatest value in this set less than or equal to the specified value if order isn't reversed, and
+     * the least value in this set greater than or equal to the specified value otherwise.
+     *
+     * @param value specified value
+     * @return found value if there is such value, and null otherwise
+     */
+    @Nullable
+    public E floor(@NotNull E value) {
+        if (reverse) {
+            return normalOrderCeiling(value);
+        } else {
+            return normalOrderFloor(value);
+        }
+    }
+
+    /**
+     * Returns the least value in this set greater than or equal to the specified value if order isn't reversed, and
+     * the greatest value in this set less than or equal to the specified value otherwise.
+     *
+     * @param value specified value
+     * @return found value if there is such value, and null otherwise
+     */
+    @Nullable
+    public E ceiling(@NotNull E value) {
+        if (reverse) {
+            return normalOrderLower(value);
+        } else {
+            return normalOrderCeiling(value);
+        }
+    }
+
+    /**
+     * Returns the least value in this set strictly greater than the given value if order isn't reversed, and
+     * the greatest value in this set strictly less than the specified value otherwise.
+     *
+     * @param value specified value
+     * @return found value if there is such value, and null otherwise
+     */
+    @Nullable
+    public E higher(@NotNull E value) {
+        if (reverse) {
+            return normalOrderLower(value);
+        } else {
+            return normalOrderHigher(value);
+        }
+    }
+
+    /**
+     * Returns an iterator over the values in this set.
+     */
+    @NotNull
+    public Iterator<E> iterator() {
+        return new MyTreeSetIterator();
+    }
+
+    /**
+     * Returns number of stored values in the tree.
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Node for binary search tree. Stores left, right children and value.
+     */
+    private class Node {
+        private Node left = null;
+        private Node right = null;
+        private E value;
+
+        /**
+         * Initializes stored value with specified value.
+         *
+         * @param value specified value
+         */
+        private Node(@NotNull E value) {
+            this.value = value;
+        }
+
+    }
+
+    /**
+     * Iterator for MyTreeSet.
+     */
+    private class MyTreeSetIterator implements Iterator<E> {
+        private E next = first();
+
+        @Override
+        public boolean hasNext() {
+            return next != null;
+        }
+
+        @Override
+        public E next() {
+            if (next == null) {
+                throw new NoSuchElementException();
+            }
+
+            E prev = next;
+            next = higher(next);
+            return prev;
+        }
+    }
+}
---
+++ MyTreeSet/src/test/java/MyTreeSetTest.java
@@ -0,0 +1,414 @@
+import org.junit.Test;
+
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.junit.Assert.*;
+
+public class MyTreeSetTest {
+    @Test
+    public void testConstructor() {
+        new MyTreeSet<String>();
+    }
+
+    @Test
+    public void testConstructorWithComparator() {
+        new MyTreeSet<Character>((o1, o2) -> -1 * o1.compareTo(o2));
+    }
+
+    @Test
+    public void testAddReturnTrue() {
+        MyTreeSet<Long> mySet = new MyTreeSet<>();
+        assertTrue(mySet.add(1L));
+    }
+
+    @Test
+    public void testAddReturnFalse() {
+        MyTreeSet<Long> mySet = new MyTreeSet<>();
+        mySet.add(1L);
+
+        assertFalse(mySet.add(1L));
+    }
+
+    @Test
+    public void testAdd() {
+        MyTreeSet<Long> mySet = new MyTreeSet<>();
+        mySet.add(17L);
+        mySet.add(13L);
+        mySet.add(2L);
+
+        assertArrayEquals(new Long[]{2L, 13L, 17L}, mySet.toArray());
+    }
+
+    @Test
+    public void testAddTheSame() {
+        MyTreeSet<Long> mySet = new MyTreeSet<>();
+        mySet.add(17L);
+        mySet.add(13L);
+        mySet.add(2L);
+        mySet.add(13L);
+
+        assertArrayEquals(new Long[]{2L, 13L, 17L}, mySet.toArray());
+    }
+
+    @Test
+    public void testAddRandom() {
+        MyTreeSet<Integer> mySet = new MyTreeSet<>();
+        TreeSet<Integer> random = new TreeSet<>();
+
+        for (int i = 0; i < 100; i++) {
+            Integer valueToAdd = ThreadLocalRandom.current().nextInt(0, 30);
+            random.add(valueToAdd);
+            mySet.add(valueToAdd);
+        }
+
+        assertArrayEquals(random.toArray(), mySet.toArray());
+    }
+
+    @Test
+    public void testRemoveReturnTrue() {
+        MyTreeSet<Character> mySet = new MyTreeSet<>((o1, o2) -> -1 * o1.compareTo(o2));
+        mySet.add((char) 1);
+
+        assertTrue(mySet.remove((char) 1));
+    }
+
+    @Test
+    public void testRemoveReturnFalse() {
+        MyTreeSet<Character> mySet = new MyTreeSet<>((o1, o2) -> -1 * o1.compareTo(o2));
+        mySet.add((char) 1);
+
+        assertFalse(mySet.remove((char) 2));
+    }
+
+    @Test
+    public void testRemove() {
+        MyTreeSet<Character> mySet = new MyTreeSet<>();
+        mySet.add('a');
+        mySet.add('z');
+        mySet.add('v');
+        mySet.add('t');
+        mySet.remove('t');
+        mySet.remove('a');
+
+        assertArrayEquals(new Character[]{'v', 'z'}, mySet.toArray());
+    }
+
+    @Test
+    public void testRemoveTheSame() {
+        MyTreeSet<Character> mySet = new MyTreeSet<>();
+        mySet.add('a');
+        mySet.add('z');
+        mySet.add('v');
+        mySet.add('t');
+        mySet.remove('t');
+        mySet.remove('a');
+        mySet.remove('t');
+
+        assertArrayEquals(new Character[]{'v', 'z'}, mySet.toArray());
+    }
+
+    @Test
+    public void testRemoveRandom() {
+        MyTreeSet<Integer> mySet = new MyTreeSet<>();
+        TreeSet<Integer> random = new TreeSet<>();
+
+        for (int i = 0; i < 100; i++) {
+            Integer valueToAdd = ThreadLocalRandom.current().nextInt(0, 30);
+            random.add(valueToAdd);
+            mySet.add(valueToAdd);
+        }
+
+        for (int i = 0; i < 30; i++) {
+            Integer valueToRemove = ThreadLocalRandom.current().nextInt(0, 30);
+            random.remove(valueToRemove);
+            mySet.remove(valueToRemove);
+        }
+
+        assertArrayEquals(random.toArray(), mySet.toArray());
+    }
+
+    @Test
+    public void testSizeEmpty() {
+        assertEquals(0, new MyTreeSet<Integer>().size());
+    }
+
+    @Test
+    public void testSizeAdd() {
+        MyTreeSet<Integer> mySet = new MyTreeSet<>();
+        mySet.add(5);
+        mySet.add(2);
+
+        assertEquals(2, mySet.size());
+    }
+
+    @Test
+    public void testSizeAddTheSame() {
+        MyTreeSet<Integer> mySet = new MyTreeSet<>();
+        mySet.add(2);
+        mySet.add(2);
+
+        assertEquals(1, mySet.size());
+    }
+
+    @Test
+    public void testSizeRemove() {
+        MyTreeSet<Integer> mySet = new MyTreeSet<>();
+        mySet.add(5);
+        mySet.add(2);
+        mySet.remove(2);
+
+        assertEquals(1, mySet.size());
+    }
+
+    @Test
+    public void testSizeRemoveTheSame() {
+        MyTreeSet<Integer> mySet = new MyTreeSet<>();
+        mySet.add(5);
+        mySet.add(2);
+        mySet.remove(2);
+        mySet.remove(2);
+
+        assertEquals(1, mySet.size());
+    }
+
+    @Test
+    public void testSizeOneHundred() {
+        MyTreeSet<Integer> mySet = new MyTreeSet<>();
+        for (int i = 0; i < 100; i++) {
+            mySet.add(i);
+        }
+
+        assertEquals(100, mySet.size());
+    }
+
+    @Test
+    public void testContainsNotExisting() {
+        MyTreeSet<Byte> mySet = new MyTreeSet<>();
+        mySet.add((byte) 1);
+
+        assertFalse(mySet.contains((byte) 2));
+    }
+
+    @Test
+    public void testContainsAdd() {
+        MyTreeSet<Byte> mySet = new MyTreeSet<>();
+        mySet.add((byte) 55);
+
+        assertTrue(mySet.contains((byte) 55));
+    }
+
+    @Test
+    public void testContainsRemove() {
+        MyTreeSet<Byte> mySet = new MyTreeSet<>();
+        mySet.add((byte) 111);
+        mySet.remove((byte) 111);
+
+        assertFalse(mySet.contains((byte) 111));
+    }
+
+    @Test
+    public void testFirstEmptySet() {
+        assertNull(new MyTreeSet<Short>((o1, o2) -> -1 * o1.compareTo(o2)).first());
+    }
+
+    @Test
+    public void testFirst() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>((o1, o2) -> -1 * o1.compareTo(o2));
+        mySet.addAll(Arrays.asList((short) 100, (short) -2, (short) -33, (short) 4));
+        assertEquals(100, mySet.first().shortValue());
+    }
+
+    @Test
+    public void testFirstDescendingSet() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>((o1, o2) -> -1 * o1.compareTo(o2));
+        mySet.addAll(Arrays.asList((short) 100, (short) -2, (short) -33, (short) 4));
+        assertEquals(-33, mySet.descendingSet().first().shortValue());
+    }
+
+    @Test
+    public void testLastEmptySet() {
+        assertNull(new MyTreeSet<Short>((o1, o2) -> -1 * o1.compareTo(o2)).last());
+    }
+
+    @Test
+    public void testLast() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>((o1, o2) -> -1 * o1.compareTo(o2));
+        mySet.addAll(Arrays.asList((short) 100, (short) -2, (short) -33, (short) 4));
+        assertEquals(-33, mySet.last().shortValue());
+    }
+
+    @Test
+    public void testLastDescendingSet() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>((o1, o2) -> -1 * o1.compareTo(o2));
+        mySet.addAll(Arrays.asList((short) 100, (short) -2, (short) -33, (short) 4));
+        assertEquals(100, mySet.descendingSet().last().shortValue());
+    }
+
+    @Test
+    public void testEmptyHigher() {
+        assertNull(new MyTreeSet<Short>().higher((short) 15));
+    }
+
+    @Test
+    public void testNotExistingHigher() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertNull(mySet.higher((short) 15));
+    }
+
+    @Test
+    public void testNotExistingHigherDescendingSet() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertNull(mySet.descendingSet().higher((short) 1));
+    }
+
+    @Test
+    public void testHigher() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertEquals(10, mySet.higher((short) 3).shortValue());
+    }
+
+    @Test
+    public void testHigherDescendingSet() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertEquals(3, mySet.descendingSet().higher((short) 10).shortValue());
+    }
+
+    @Test
+    public void testEmptyLower() {
+        assertNull(new MyTreeSet<Short>().lower((short) 15));
+    }
+
+    @Test
+    public void testNotExistingLower() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertNull(mySet.lower((short) 1));
+    }
+
+    @Test
+    public void testNotExistingLowerDescendingSet() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertNull(mySet.descendingSet().lower((short) 15));
+    }
+
+    @Test
+    public void testLower() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertEquals(1, mySet.lower((short) 2).shortValue());
+    }
+
+    @Test
+    public void testLowerDescendingSet() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertEquals(10, mySet.descendingSet().lower((short) 4).shortValue());
+    }
+
+    @Test
+    public void testEmptyFloor() {
+        assertNull(new MyTreeSet<>().floor((short) 15));
+    }
+
+    @Test
+    public void testNotExistingFloor() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertNull(mySet.floor((short) 0));
+    }
+
+    @Test
+    public void testNotExistingFloorDescendingSet() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertNull(mySet.descendingSet().floor((short) 17));
+    }
+
+    @Test
+    public void testFloor() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertEquals(15, mySet.floor((short) 15).shortValue());
+    }
+
+    @Test
+    public void testFloorDescendingSet() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertEquals(10, mySet.descendingSet().lower((short) 4).shortValue());
+    }
+
+    @Test
+    public void testEmptyCeiling() {
+        assertNull(new MyTreeSet<>().ceiling((short) 15));
+    }
+
+    @Test
+    public void testNotExistingCeiling() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertNull(mySet.ceiling((short) 100));
+    }
+
+    @Test
+    public void testNotExistingCeilingDescendingSet() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertNull(mySet.descendingSet().ceiling((short) -17));
+    }
+
+    @Test
+    public void testCeiling() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertEquals(15, mySet.ceiling((short) 15).shortValue());
+    }
+
+    @Test
+    public void testCeilingDescendingSet() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 15, (short) 10));
+        assertEquals(3, mySet.descendingSet().ceiling((short) 4).shortValue());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testIteratorThrowsNoSuchElementException() {
+        Iterator<Short> iterator = new MyTreeSet<Short>().iterator();
+        iterator.next();
+    }
+
+    @Test
+    public void testIterator() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 2, (short) 0));
+        Iterator<Short> iterator = mySet.iterator();
+
+        for (byte i = 0; i < 4; i++) {
+            assertTrue(iterator.hasNext());
+            assertEquals(i, iterator.next().byteValue());
+        }
+
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void testDescendingIterator() {
+        MyTreeSet<Short> mySet = new MyTreeSet<>();
+        mySet.addAll(Arrays.asList((short) 1, (short) 3, (short) 2, (short) 0));
+        Iterator<Short> iterator = mySet.descendingIterator();
+
+        for (byte i = 3; i >= 0; i--) {
+            assertTrue(iterator.hasNext());
+            assertEquals(i, iterator.next().byteValue());
+        }
+
+        assertFalse(iterator.hasNext());
+    }
+
+}
